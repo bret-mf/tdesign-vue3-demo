90c5da939718cf346e8c501816813c135c2e1d15
yaoxiangyi <yao1909@163.com>
2023-11-26 01:12:26 +0800

    add env and fix scss

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./style.scss";
 import App from "./App.vue";
 
 // import "tdesign-vue-next/dist/reset.css";
+import "tdesign-vue-next/es/style/index.css";
 
 const app = createApp(App);
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
@@ -6,32 +6,36 @@ import { TDesignResolver } from "unplugin-vue-components/resolvers";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 8888,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      host: "0.0.0.0",
+      port: 8888,
     },
-  },
-  base: "/tdesign-vue3-demo/",
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [
-        TDesignResolver({
-          library: "vue-next",
-        }),
-      ],
-    }),
-    Components({
-      resolvers: [
-        TDesignResolver({
-          library: "vue-next",
-        }),
-      ],
-    }),
-  ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    base: env.VITE_APP_BASE_PATH,
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [
+          TDesignResolver({
+            library: "vue-next",
+          }),
+        ],
+      }),
+      Components({
+        resolvers: [
+          TDesignResolver({
+            library: "vue-next",
+          }),
+        ],
+      }),
+    ],
+  };
 });
